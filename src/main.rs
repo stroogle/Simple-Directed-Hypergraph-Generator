1c0953ed53e42e19ebf84889ebbecea31d17f526
Colm Moore <contact@colm.tech>
2025-04-03 19:33:52 +1100

    parsing command line args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(version, about)]
+struct Args {
+
+    #[arg(short, long)]
+    left_column_size: u32,
+
+    #[arg(short, long)]
+    right_column_size: u32,
+
+    #[arg(short, long)]
+    generate_n_graphs: u8
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args = Args::parse();
+    println!(
+        "Left column size of {}, Right column size of {}, {} times", 
+        &args.left_column_size,
+        &args.right_column_size,
+        &args.generate_n_graphs
+    );
 }
